8b6cc380bacf193b285ea55bc8e90acd2d5e53ae
Piotr Domagalski <pdomagalski02@gmail.com>
2026-05-11 14:54:41 +0200

    integrate ktor project as submodule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,17 +11,24 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
+
+    // from ktor
+    versionCatalogs {
+        create("ktorLibs").from("io.ktor:ktor-version-catalog:3.4.0")
+    }
 }
 
 rootProject.name = "Routes App"
 include(":app")
- 
+include(":backend")
